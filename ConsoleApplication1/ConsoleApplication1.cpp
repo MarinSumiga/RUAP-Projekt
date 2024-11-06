008c8267fa02cc4bd11326f1cee63a0bfa7e13c1
MarinSumiga <sumiga1988@gmail.com>
2024-11-06 09:40:21 +0100

    Zad1 commit2 added indeks

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Pozdrav iz aplikacije od Marin Šumiga\n";
+    std::cout << "Pozdrav iz aplikacije od Marin Šumiga, broj indeksa: 601983\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
